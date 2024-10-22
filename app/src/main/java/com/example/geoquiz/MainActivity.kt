8e19c3fe66f1085bfd96b3233ba835d9e5ae3d30
alyavit <alyavit1@gmail.com>
2024-10-22 19:52:06 +0700

    Изменение структуры ресурсов strings.xml. Создание класса Question.kt. Создание списка экземпляров указанного класса, для задания списка вопросов с ответами формата "вопрос-ответ". + при нажатии кнопки btnNext изменение вопроса на следующий в списке.

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -7,6 +7,21 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private val questionBank = listOf(
+        Question(R.string.question_first, true),
+        Question(R.string.question_second,
+            true),
+        Question(R.string.question_third,
+            false),
+        Question(R.string.question_fourth,
+            false),
+        Question(R.string.question_fivth
+            , true),
+        Question(R.string.question_second,
+            true))
+    private var currentIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,9 +30,17 @@ class MainActivity : AppCompatActivity() {
         val btnNext = findViewById<Button>(R.id.btnNext)
         val btnYes = findViewById<Button>(R.id.btnYes)
         val btnNo = findViewById<Button>(R.id.btnNo)
-
         var correctAnswers: Int = 0
             //TODO: дочитать главы книги. дописать логику и работу приложения
+        val questionTextResId = questionBank[currentIndex].textResId
+        textQ.setText(questionTextResId)
+
+        btnNext.setOnClickListener {
+            currentIndex = (currentIndex + 1) %
+                    questionBank.size
+            val questionTextResId = questionBank[currentIndex].textResId
+            textQ.setText(questionTextResId)
+        }
     }
 
     fun correctToast() {
